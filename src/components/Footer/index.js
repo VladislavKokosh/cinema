82950098e359818b9ae3970104ad96a306b05d06
iamkoks <iamkoks@users.noreply.github.com>
2020-08-27 13:46:40 +0300

    create footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Typography } from 'antd';
+
+const { Text, Title } = Typography;
+
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+                <Title level={3}>Мы всегда готовы вам помочь - звонити или пишите.</Title>
+        </footer>
+    )
+}
+
+export default Footer
