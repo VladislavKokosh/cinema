14bff44c68f6d2aeb2b50f7cadbe04876b194abc
iamkoks <iamkoks@users.noreply.github.com>
2020-08-27 15:30:30 +0300

    fix scss Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,9 +9,10 @@ const Footer = () => {
     return (
         <footer className ='footer'>
             <div className ='footer__context'>
-                <Title level={5}>Мы всегда готовы вам помочь - звоните или пишите.</Title>
-                <Text strong>8 800 600-30-30</Text>
-                <hr/>
+                <div className='footer__context-about'>
+                    <Title level={5}>Мы всегда готовы вам помочь - звоните или пишите.</Title>
+                    <Text strong>8 800 600-30-30</Text>
+                </div>
                 <Text>© 2003–2020, Звезда</Text>
             </div>
         </footer>
