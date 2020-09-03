2fe0916e1a1e10387e534052696d7eb42fbfc82c
iamkoks <iamkoks@users.noreply.github.com>
2020-09-03 12:31:35 +0300

    time cinema

--- src/components/AboutFilm/index.js
+++ src/components/AboutFilm/index.js
@@ -1,32 +1,43 @@
 import React, { useEffect, useState } from 'react'
 
-import { Image, Typography } from 'antd'
+import { Image, List, Typography, Divider } from 'antd'
 import './index.scss'
 import films from '../../films.json'
+import times from '../../time.json'
 
 const { Title, Paragraph } = Typography;
 
 const AboutFilm = (props) => {
 
     const getFilmById = (id) => {
-        const film = films.find(film =>
-            film.id === id);
+        const film = [];
+        films.map(fFilm => fFilm.id === id ? film+=fFilm : console.log(""))
         return film
     }
 
-    const [film, setFilm] = useState(null)
+    const getTimeById = (id) => {
+       const result = times?.filter(time => time.id === id);
+       return result
+    }
+
+    const [film, setFilm] = useState(null);
+    const [time, setTime] = useState(null);
 
     useEffect(()=> {
         setFilm(getFilmById(props.match.params.id))
     }, [])
-    console.log(film)
+
+    useEffect(()=> {
+        setTime(getTimeById(props.match.params.id))
+    }, [])
 
     return(
         <div className="">
+
             <div className="about__film">
                 <div className="-image">
                     <Image
-                        width={200}
+                        width={270}
                         src={film?.cover}
                     />
                 </div>
@@ -40,7 +51,18 @@ const AboutFilm = (props) => {
                 </div>
             </div>
             <div className="-table">
-
+                <Divider orientation="left">Время и дата сеансов</Divider>
+                    <List
+                    header={<div>Header</div>}
+                    footer={<div>Footer</div>}
+                    bordered
+                    dataSource={time}
+                    renderItem={item => (
+                        <List.Item>
+                        <Typography.Text mark>[ITEM]</Typography.Text> {item.id}
+                        </List.Item>
+                    )}
+                />
             </div>
         </div>
     )
