5affa8fe8349dd73ed0bd6577693c9e72704e42a
iamkoks <iamkoks@users.noreply.github.com>
2020-09-16 13:51:38 +0300

    scss for tickets and ticket components

--- src/components/Ticket/index.js
+++ src/components/Ticket/index.js
@@ -2,9 +2,23 @@ import React from 'react'
 import moment from 'moment'
 import 'moment/locale/ru'
 
-const Ticket = ({film, date, hall, seat, row, cost}) => {
+import { Card } from 'antd';
+import { Steps } from 'antd';
+import './index.scss'
+const { Step } = Steps;
+const { Meta } = Card;
+
+
+
+const Ticket = ({film, filmDuration, date, hall, seat, row, cost}) => {
 
     const getTime = (time) => {
+        var ticketTime = moment(time);
+        ticketTime.local('ru');
+        return ticketTime.format('LT')
+    }
+
+    const getDate = (time) => {
         var ticketTime = moment(time);
         ticketTime.local('ru');
         return ticketTime.format('LLLL')
@@ -12,7 +26,37 @@ const Ticket = ({film, date, hall, seat, row, cost}) => {
 
     return(
         <div className='ticket'>
-            {film} , {getTime(+date)} ,{hall} , {seat}, {row}, {cost}
+            <Card
+                hoverable
+                style={{ width: 600 }}
+            >
+                <Meta title={film}/>
+                <div className="ticket__about">
+                    <div className="ticket__about-hall">
+                        Зал: {hall}
+                    </div>
+                    <div className="ticket__about-row">
+                        Ряд: {row}
+                    </div>
+                    <div className="ticket__about-seat">
+                        Место: {seat}
+                    </div>
+                    <div className="ticket__about-time">
+                        <Steps direction="vertical" progressDot current={2}>
+                            <Step title={getTime(+date)} description="Начало фильма."/>
+                            <Step title={getTime(+date + +filmDuration)} description="Конец фильма." />
+                        </Steps>
+                    </div>
+                </div>
+                <div className="ticket__footer">
+                    <div className="ticket__footer-date">
+                        {getDate(+date)} {cost} блр. руб.
+                    </div>
+                    <div className="ticket__footer-cinema">
+                        Звезда
+                    </div>
+                </div>
+            </Card>
         </div>
     )
 }
--- src/components/Tickets/index.js
+++ src/components/Tickets/index.js
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useSelector } from 'react-redux'
 
+import { Typography, Button } from 'antd';
+
 import Ticket from '../Ticket'
+import './index.scss'
+
+const { Title } = Typography;
 
 const Tickets = () => {
     const film = useSelector(state => state.films.filmById)
     const hall = useSelector(state => state.halls.hallById)
     const session = useSelector(state => state.sessions.sessionById)
     const places = useSelector(state => state.places.choisePlaces)
+    const choisePlaces = useSelector(state => state.places.choisePlaces)
+    const [costSum, setCostSum] = useState(0)
 
+    useEffect(() => {
+        const sum = choisePlaces.reduce((sum, value) => sum + +value.cost, 0)
+        setCostSum(sum)
+    }, [choisePlaces])
     return(
-        <div className='tickets'>
-            {places?.map((place, index) => (
-                <Ticket
-                    film={film.name}
-                    date={session.date}
-                    hall={hall.title}
-                    seat={place.seat}
-                    row={place.row}
-                    cost={place.cost}
-                    key={index}
-                />)
-            )}
+        <div className="ordering">
+            <div className='__tickets'>
+                {places?.map((place, index) => (
+                    <Ticket
+                        film={film.name}
+                        filmDuration={film.duration}
+                        date={session.date}
+                        hall={hall.title}
+                        seat={place.seat}
+                        row={place.row}
+                        cost={place.cost}
+                        key={index}
+                    />)
+                )}
+            </div>
+            <div className="__purchase">
+                <Title level={5}>Стоимость билетов: {costSum} блр. руб.</Title>
+                <Button type="primary">Купить</Button>
+            </div>
         </div>
     )
 }
